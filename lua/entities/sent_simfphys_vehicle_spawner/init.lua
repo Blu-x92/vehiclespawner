f70ca4ff7014bd4379766996e9dca43f9082dd4a
Blu <swagnemite@gmx.de>
2016-12-30 15:42:10 +0100

    weapon support

--- lua/entities/sent_simfphys_vehicle_spawner/init.lua
+++ lua/entities/sent_simfphys_vehicle_spawner/init.lua
@@ -136,6 +136,26 @@ function ENT:CreateVehicle( vname, pos, ang )
 			Ent:setDoorGroup(self.team)
 		end
 	end )
+	
+	
+	if (Ent:GetModel( ) == "models/apc/apc.mdl") then
+		timer.Simple( 0.2, function()
+			if (!IsValid(vehicle)) then return end
+			if (vname == "sim_fphys_conscriptapc_armed") then
+				table.insert(armedAPCSTable, Ent)
+			end
+		end)
+	end
+	
+	if (Ent:GetModel( ) == "models/vehicles/buggy_elite.mdl") then
+		timer.Simple( 0.2, function()
+			if (!IsValid(vehicle)) then return end
+			if (vname == "sim_fphys_v8elite_armed") then
+				table.insert(armedJEEPSTable, Ent)
+				vehicle:SetBodygroup(1,1)
+			end
+		end)
+	end
 end
 
 function ENT:Think()
